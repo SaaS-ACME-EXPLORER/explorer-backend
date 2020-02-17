47b026c51230cc6bf891102d7eb262689149eb3b
Andrés López Albín <andres.albin93@gmail.com>
2020-02-17 20:18:18 +0100

    bd crashed

--- app/controllers/actorController.js
+++ app/controllers/actorController.js
@@ -1,28 +1,28 @@
 'use strict';
 /*---------------ACTOR----------------------*/
 var mongoose = require('mongoose'),
-  Actor = mongoose.model('Actor');
+    Actor = mongoose.model('Actor');
 
-exports.list_all_actors = function(req, res) {
+exports.list_all_actors = function (req, res) {
     res.status(200).send("OK");
 };
 
-exports.create_an_actor = function(req, res) {
+exports.create_an_actor = function (req, res) {
     res.status(200).send("OK");
 };
 
-exports.read_an_actor = function(req, res) {
+exports.read_an_actor = function (req, res) {
     res.status(200).send("OK");
 };
 
-exports.update_an_actor = function(req, res) {
+exports.update_an_actor = function (req, res) {
     res.status(200).send("OK");
 };
 
-exports.delete_an_actor = function(req, res) {
+exports.delete_an_actor = function (req, res) {
     res.status(200).send("OK");
 };
 
-exports.change_an_actor_status = function(req, res) {
+exports.change_an_actor_status = function (req, res) {
     res.status(200).send("OK");
 };
