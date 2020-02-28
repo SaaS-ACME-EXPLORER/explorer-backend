6ad7b5205c9358617d2a03b405eadf0aeb8e5e74
balramrom <balramrom@alumn.us.es>
2020-02-28 14:10:26 +0100

    Added implementations to read, pay and change the status of an application (with validators)

--- app/controllers/applicationsController.js
+++ app/controllers/applicationsController.js
@@ -165,7 +165,7 @@ exports.read_an_application = function (req, res) {
   Application.findById(applicationId, async function (err, application) {
     if (err) {
       res.status(500).send(err);
-    }else if(application == null){
+    } else if (application == null) {
       logger.error(`Could not find an application with id ${applicationId}`);
       res.status(404).json("Could not find an application with id" + applicationId);
     } else {
@@ -211,56 +211,18 @@ const displayManagerApplication = function (res, application, actorId) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/*------------------PAY AN APPLICATION----------------------*/
 
 
 //An application with status “DUE” changes automatically to status “ACCEPTED” whenever the corresponding applicant pays the trip
 //body: {paid: 'true', actorId: '123abc'}
 //params: applicationId
-exports.pay_an_application = function (req, res) {
+exports.pay_an_application = async function (req, res) {
   //Check that the user is explorer and it is the user that made the application. (403)
   const body = req.body;
-  const applicationId = req.params._id;
-  payAnApplicationValidator(applicationId, body).then(
+  const applicationId = req.params.app_id;
+  let errors = await payAnApplicationValidator(applicationId, body);
+  if (errors.length == 0) {
     Application.findOneAndUpdate({ _id: applicationId }, { paid: body.paid, status: "ACCEPTED" }, { new: true }, function (err, editedApplication) {
       if (err) {
         if (err.name == 'ValidationError') {
@@ -273,26 +235,82 @@ exports.pay_an_application = function (req, res) {
       else {
         res.status(200).json(editedApplication);
       }
-    })
-  ).catch((error) => {
-    if (error.errors.length() == 1 && error.errors.contains('Ivalid role for the actor. Must be EXPLORER')) {
+    });
+  } else {
+    if (errors.length == 1 && errors.includes('Ivalid role for the actor. Must be EXPLORER')) {
       res.status(403);
     } else {
       res.status(400);
     }
-    res.send(error);
-  });
-
+    res.json({ "errors": errors });
+  }
 };
+
+// body: {paid: 'true', actorId: '123abc'}
+var payAnApplicationValidator = async function (applicationId, body) {
+
+  var errors = [];
+  if (Object.keys(body).length != 2 || !body.actorId || !body.paid) {
+    if (!body.actorId) {
+      logger.error('Attribute actorId required');
+      errors.push('Attribute actorId required');
+    }
+    if (Object.keys(body).length != 2) {
+      logger.error('More or less attributes than expected');
+      errors.push('More or less attributes than expected');
+    }
+    if (!body.paid) {
+      logger.error('The value of the attribute paid must be true');
+      errors.push('The value of the attribute paid must be true');
+    }
+  } else {
+    let actorRole = await ActorUtil.getRoleById(body.actorId);
+    if (!actorRole) {
+      logger.error(`Error when searching an actor with id ${body.actorId}`);
+      errors.push("Error when searching an actor with id" + body.actorId);
+    } else if (actorRole != 'EXPLORER') {
+      logger.error('Ivalid role for the actor. Must be EXPLORER');
+      errors.push('Ivalid role for the actor. Must be EXPLORER');
+    } else {
+      await Application.findById(applicationId, function (error, application) {
+        if (error || !application) {
+          logger.error(`Error searching an application with id ${applicationId}.`);
+          errors.push('Error searching an application with id ' + applicationId);
+        }
+        if (application.status != "DUE") {
+          logger.error(`Invalid application status: ${application.status}, must be DUE.`);
+          errors.push('Invalid application status: ' + application.status + ', must be DUE.');
+        }
+        if (application.paid) {
+          logger.error("The application for the trip has been already paid");
+          errors.push('The application for the trip has been already paid');
+        }
+        if (application.explorerId != body.actorId) {
+          logger.error(`The actor did not apply for the trip with id ${application.tripId}`);
+          errors.push('The actor did not apply for the trip with id ' + application.tripId);
+        }
+      });
+    }
+  }
+  return errors;
+}
+
+
+
+
+
+/*------------------CHANGE AN APPLICATION STATUS----------------------*/
+
 
 //Managers: Can cange status from “PENDING” to “REJECTED” (reason why) or from “PENDING” to “DUE”
 //Explorer: Cancel an application with status “PENDING”
 //Summary: update state and its reason if it is cancel or rejected;
 //body: {status: "REJECTED"||"DUE"||"CANCELLED", statusReason: "abc", actorId: "123abc"}
-exports.change_an_application_status = function (req, res) {
+exports.change_an_application_status = async function (req, res) {
   const applicationId = req.params.app_id;
   const body = req.body;
-  changeAnApplicationStatusValidator(applicationId, body).then(
+  let errors = await changeAnApplicationStatusValidator(applicationId, body);
+  if (errors.length == 0) {
     Application.findOneAndUpdate({ _id: applicationId }, body, { new: true }, function (err, editedApplication) {
       if (err) {
         if (err.name == 'ValidationError') {
@@ -305,144 +323,99 @@ exports.change_an_application_status = function (req, res) {
       else {
         res.status(200).json(item);
       }
-    })
-  ).catch((error) => {
-    if (error.errors.length == 1 && (error.errors.contains("The actor does not manage the application, because he does not manage the trip") || error.errors.contains("The actor did not make the application"))) {
+    });
+  } else {
+    if (errors.length == 1 && (errors.includes("The actor does not manage the application, because he does not manage the trip") || errors.includes("The actor did not make the application"))) {
       res.status(403);
     } else {
       res.status(400);
     }
     res.send(error);
-  });
+  }
 };
-
-
-//VALIDATORS
-
-var payAnApplicationValidator = function (applicationId, body) {
-
-  return new Promise(async function (resolve, reject) {
-    var errors = [];
-    if (Object.keys(body).length != 2 || !body.actorId || !body.paid) {
-      if (!body.actorId) {
-        logger.error('Attribute actorId required');
-        errors.push('Attribute actorId required');
-      }
-      if (Object.keys(body).length != 2) {
-        logger.error('More or less attributes than expected');
-        errors.push('More or less attributes than expected');
-      }
-      if (!body.paid) {
-        logger.error('The value of the attribute paid must be true');
-        errors.push('The value of the attribute paid must be true');
-      }
-    } else {
-      await Actor.findById(body.actorId, function (error, actor) {
-        if (error) {
-          logger.error(`Error searching an actor with id ${body.actorId}.`);
-          errors.push('Error searching an actor with id' + body.actorId);
-        } else if (!actor.role === 'EXPLORER') {
-          logger.error('Ivalid role for the actor. Must be EXPLORER');
-          errors.push('Ivalid role for the actor. Must be EXPLORER');
-        } else {
-          Application.findById(applicationId, function (error, application) {
-            if (error) {
-              logger.error(`Error searching an application with id ${applicationId}.`);
-              errors.push('Error searching an application with id ' + applicationId);
-            }
-            else if (application.status != "DUE") {
-              logger.error(`Invalid application status: ${application.status}, must be DUE.`);
-              errors.push('Invalid application status: ' + application.status + ', must be DUE.');
-            }
-            else if (application.paid) {
-              logger.error("The application for the trip has been already paid");
-              errors.push('The application for the trip has been already paid');
-            }
-            else if (application.explorerId != body.actorId) {
-              logger.error(`The actor did not apply for the trip with id ${application.tripId}`);
-              errors.push('The actor did not apply for the trip with id ' + application.tripId);
-            }
-          });
-        }
-      });
-    }
-    //Necesario el async - await anterior para que no se ejecute antes de tiempo 
-    if (errors.length > 0) {
-      reject({ "errors": errors });
-    } else {
-      resolve();
-    }
-
-  });
-}
-
 
 
 //Managers: Can cange status from “PENDING” to “REJECTED” (reason why) or from “PENDING” to “DUE”
 //Explorer: Cancel an application with status “PENDING” or "ACCEPTED"
 //body: {status: "REJECTED"||"DUE"||"CANCELLED", statusReason: "abc", actorId: "123abc"}
-var changeAnApplicationStatusValidator = function (applicationId, body) {
-  return new Promise(async function (resolve, reject) {
-    var errors = [];
-    if (!body.status || !body.actorId) {
-      logger.error("Application status and actor id must be provided");
-      errors.push("Application status and actor id must be provided");
-    } else {
-      await Application.findById(applicationId, function (error, application) {
-        if (error) {
-          logger.error(`Error when searching for an application with id ${applicationId}`);
-          errors.push("Error when searching for an application with id " + applicationId);
-        } else {
-          Actor.findById(actorId, function (error, actor) {
-            if (error) {
-              logger.error(`Error when searching an actor with id ${actorId}`);
-              errors.push("Error when searching an actor with id " + actorId);
-            } else if (actor.role === 'MANAGER') { //MANAGERS
-              if (application.status === 'PENDING') {
-                Trip.find({ ticker: application.tripId }, function (error, trip) {
-                  if (error) {
-                    logger.error(`Error when searching for a trip with id ${application.tripId}`);
-                    errors.push("Error when searching for a trip with id " + application.tripId);
-                  } else {
-                    if (trip.managedBy != actorId) {
-                      logger.error(`The actor with id ${actorId}, does not manage the trip with id ${trip.ticker}`);
-                      error.push("The actor does not manage the application, because he does not manage the trip");
-                    } else if (body.status != "DUE" || body.status != "REJECTED") {
-                      logger.error(`Invalid application status ${body.status}, must be DUE or REJECTED`);
-                      error.push("Application status must be DUE or REJECTED");
-                    } else if (body.status === "REJECTED" && !body.statusReason) {
-                      logger.error("If the new application status is REJECTED, a reason must be provided");
-                      error.push("If the new application status is REJECTED, a reason must be provided");
-                    }
-                  }
-                });
-              } else {
-                logger.error("The application state must be PENDING");
-                errors.push("The application state must be PENDING");
-              }
-            } else if (actor.role === 'EXPLORER') { //EXPLORERS
-              if (application.explorerId != actorId) {
-                logger.error(`The actor with id ${actorId}, did not make the application`);
-                errors.push("The actor did not make the application");
-              } else if (body.status != 'CANCELLED') {
-                logger.error("The explorer can only cancell the application");
-                errors.push("The explorer can only cancell the application");
-              } else if (application.status != 'PENDING' || application.status != 'ACCEPTED') {
-                logger.error("An explorer only can cancell an application with status PENDING  or ACCEPTED");
-                errors.push("An explorer only can cancell an application with status PENDING  or ACCEPTED");
-              }
-            }
-          });
+var changeAnApplicationStatusValidator = async function (applicationId, body) {
+  var errors = [];
+  if (!body.status || !body.actorId) {
+    logger.error("Application status and actor id must be provided");
+    errors.push("Application status and actor id must be provided");
+  } else {
+    await Application.findById(applicationId, async function (error, application) {
+      if (error) {
+        logger.error(`Error when searching for an application with id ${applicationId}`);
+        errors.push("Error when searching for an application with id " + applicationId);
+      } else {
+        let actorRole = await ActorUtil.getRoleById(body.actorId);
+        if (!actorRole) {
+          logger.error(`Error when searching an actor with id ${body.actorId}`);
+          errors.push("Error when searching an actor with id " + body.actorId);
+        } else if(actorRole === 'MANAGER'){ //MANAGERS
+          let managerValidationErrors = await changeAnApplicationStatusByManagerValidator(application, body);
+          if(managerValidationErrors != ""){
+            errors.push(managerValidationErrors);
+          }
+        }else if(actorRole === 'EXPLORER'){ //EXPLORERS
+          let explorerValidationErrors = changeAnApplicationStatusByExplorerValidator(application, body);
+          if(explorerValidationErrors != ""){
+            errors.push(explorerValidationErrors);
+          }
+        }else{
+          logger.error(`Invalid actor. Must be an explorer or a manager`);
+          errors.push("Invalid actor. Must be an explorer or a manager");
         }
-      });
-    }
-
-    //Necesario el async - await anterior para que no se ejecute antes de tiempo 
-    if (errors.length > 0) {
-      reject({ "errors": errors });
-    } else {
-      resolve();
-    }
-
-  });
+      }
+    });
+  }
+  return errors;
 }
+
+//Managers: Can cange status from “PENDING” to “REJECTED” (reason why) or from “PENDING” to “DUE”
+//body: {status: "REJECTED"||"DUE", statusReason: "abc", actorId: "123abc"}
+var changeAnApplicationStatusByManagerValidator = async function(application, body){
+  var res = "";
+  if (application.status === 'PENDING') {
+    await Trip.find({ ticker: application.tripId }, function (error, trip) {
+      if (error) {
+        logger.error(`Error when searching for a trip with id ${application.tripId}`);
+        res = "Error when searching for a trip with id " + application.tripId;
+      } else {
+        if (trip.managedBy != actorId) {
+          logger.error(`The actor with id ${actorId}, does not manage the trip with id ${trip.ticker}`);
+          res = "The actor does not manage the application, because he does not manage the trip";
+        } else if (body.status != "DUE" || body.status != "REJECTED") {
+          logger.error(`Invalid application status ${body.status}, must be DUE or REJECTED`);
+          res = "Application status must be DUE or REJECTED";
+        } else if (body.status === "REJECTED" && !body.statusReason) {
+          logger.error("If the new application status is REJECTED, a reason must be provided");
+           res = "If the new application status is REJECTED, a reason must be provided";
+        }
+      }
+    });
+  }else{
+    logger.error("Invalid application status. Must be PENDING");
+    res = "Invalid application status. Must be PENDING";
+  }
+  return res;
+}
+
+//Explorer: Cancel an application with status “PENDING” or "ACCEPTED"
+//body: {status: "CANCELLED", statusReason: "abc", actorId: "123abc"}
+var changeAnApplicationStatusByExplorerValidator = function(application, body){
+  var res = "";
+  if (application.explorerId != actorId) {
+    logger.error(`The actor with id ${actorId}, did not make the application`);
+    res = "The actor did not make the application";
+  } else if (body.status != 'CANCELLED') {
+    logger.error("The explorer can only cancell the application");
+    res = "The explorer can only cancell the application";
+  } else if (application.status != 'PENDING' && application.status != 'ACCEPTED') {
+    logger.error("An explorer only can cancell an application with status PENDING  or ACCEPTED");
+    res = "An explorer only can cancell an application with status PENDING  or ACCEPTED";
+  }
+  return res;
+}
+
