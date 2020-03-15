95fa9a5b3156b4f41b5111d8ed3ed19b90907a08
Andrés López Albín <andres.lopezalbin.external@ts.fujitsu.com>
2020-03-15 12:46:48 +0100

    Restored firebase connection and datawarehouse cron

--- index.js
+++ index.js
@@ -19,6 +19,14 @@ app.use(bodyParser.json());
 
 var admin = require("firebase-admin");
 
+var serviceAccount = require("./acme-explorer-1bfd7-firebase-adminsdk-77ef8-20c05cb372");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://acme-explorer-1bfd7.firebaseio.com"
+});
+
+
 // Setting up database
 logger.info("Setting up database");
 
@@ -40,7 +48,7 @@ dbConnect().then(() => {
     process.exit();
 });
 
-//datawarehouse.createDataWareHouseJob()
+datawarehouse.createDataWareHouseJob()
 
 
 module.exports = app;
