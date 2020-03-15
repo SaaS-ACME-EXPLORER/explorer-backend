9acbf32135119d879b0e35450597685a9830b95d
Nono <nono.rr2@gmail.com>
2020-03-15 12:51:32 +0100

    Cambiado de POST a GET

--- app/routes/loginRoutes.js
+++ app/routes/loginRoutes.js
@@ -4,5 +4,5 @@ module.exports = function (app) {
     var actors = require('../controllers/actorController');
 
     app.route(BASE_API_PATH + '/login')
-        .post(actors.login_an_actor);
+        .get(actors.login_an_actor);
 };
